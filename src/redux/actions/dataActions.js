3f0bb99a54a2ea0b8968dcef562b4fbdb957ad2b
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-04-29 21:54:01 -0500

    Modal to add screams

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -4,6 +4,10 @@ import {
   LIKE_SCREAM,
   UNLIKE_SCREAM,
   DELETE_SCREAM,
+  CLEAR_ERRORS,
+  SET_ERRORS,
+  POST_SCREAM,
+  LOADING_UI,
 } from "../types";
 import axios from "axios";
 
@@ -16,6 +20,19 @@ export const getScreams = () => (dispatch) => {
     })
     .catch((err) => {
       dispatch({ type: SET_SCREAMS, payload: [] });
+    });
+};
+
+export const postScream = (newScream) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/scream", newScream)
+    .then((res) => {
+      dispatch({ type: POST_SCREAM, payload: res.data });
+      dispatch({ type: CLEAR_ERRORS });
+    })
+    .catch((err) => {
+      dispatch({ type: SET_ERRORS, payload: err.response.data });
     });
 };
 
